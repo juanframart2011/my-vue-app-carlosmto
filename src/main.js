81124b2ab10f336e9fbf8bbc39c145156f4a2d00
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2023-11-02 18:50:53 -0600

    ajuste de rutas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from "./router";
 
-createApp(App).mount('#app')
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/nova-vue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+
+const app = createApp(App);
+
+app.use(PrimeVue, { ripple: true });
+app.use(router).mount("#app");
